8be6e29dcbf5330004eb617f55e99bb34b7530be
Doug Stockalper <dstockalper@gmail.com>
2014-11-12 13:15:29 -0800

    user - review relation

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,5 @@
 class Review < ActiveRecord::Base
 	belongs_to :plate
+	# user below refers to reviews made BY user, not OF user
+	belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
 	has_secure_password
 	belongs_to :plate
+	# reviews below refer to reviews made by user; not of user
+	has_many :reviews
 	validates :email, presence: true, uniqueness: true
 	validates :password, presence: true
 end
